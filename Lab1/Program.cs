df0a39a4e746a65a471a59e230fc36a1a317af50
Vladisla Ivanov <vladislavivanovspb@gmail.com>
2022-05-17 21:26:41 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+// See https://aka.ms/new-console-template for more information
+{
+    string myName;
+    Console.WriteLine("Please enter your name");
+    myName = Console.ReadLine();
+    Console.WriteLine("Hello {0}", myName);
+}        
---
+++ Lab1_Exception handler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+try
+{
+    Console.WriteLine("Please enter the first integer");
+    string temp = Console.ReadLine();
+    int i = Int32.Parse(temp);
+
+    Console.WriteLine("Please enter the second integer");
+    temp = Console.ReadLine();
+    int j = Int32.Parse(temp);
+
+    int k = i / j;
+    Console.WriteLine("Division result = {0}", k);
+}
+catch (Exception e)
+{
+    Console.WriteLine("An exception was thrown: {0}", e);
+}
