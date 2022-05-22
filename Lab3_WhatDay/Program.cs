dd6f4abb0e85e041ccb552a45dddd1af864af53c
Vladisla Ivanov <vladislavivanovspb@gmail.com>
2022-05-22 17:38:27 +0300

    замена If на foreah

--- Lab3_WhatDay/Program.cs
+++ Lab3_WhatDay/Program.cs
@@ -1,157 +1,53 @@
-﻿Console.Write("Please enter a day number between 1 and 365: ");
-string line = Console.ReadLine();
-int dayNum = int.Parse(line);
-// Console.WriteLine("DayNanber = {0}", dayNum);
-int monthNum = 0;
-if (dayNum <= 31) // January
-{ 
-    goto End;
-}
-else
-{ 
-    dayNum-=31;
-    monthNum++;
-}
-if (dayNum <=28) // February
+﻿namespace Lab3_WhatDay
 {
-    goto End;
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.ICollection DaysInMonths
+        = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            Console.Write("Please enter a day number between 1 and 365: ");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+
+            int monthNum = 0;
+
+            foreach (int daysInMonth in DaysInMonths)
+            {
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
+            }
+
+        End:
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+  
+            Console.WriteLine("{0} {1}", monthName, dayNum);
+        
+
+        }
+    }
 }
-else
-{
-    dayNum -= 28;
-    monthNum++;
-}
-if (dayNum <= 31)   //March
-{
-    goto End;
-}
-else
-{
-    dayNum -= 31;
-    monthNum++;
-}
-if (dayNum <= 30) // April
-{ 
-    goto End;
-}
-else
-{ 
-    dayNum -= 30;
-    monthNum++;
-}
-if (dayNum <= 31) //May
-{
-    goto End;
-}
-else 
-{ 
-    dayNum -= 31;
-    monthNum++;
-}
-if (dayNum <=30) //June
-{
-    goto End;
-}
-else
-{
-    dayNum -= 30;
-    monthNum++;
-}
-if (dayNum <=31) //July
-{
-    goto End;
-}
-else
-{
-    dayNum -= 31;
-    monthNum++;
-}
-if (dayNum <= 31) //August
-{
-    goto End;
-}
-else
-{
-    dayNum -= 31;
-    monthNum++;
-}
-if (dayNum <= 30) //September
-{
-    goto End;
-}
-else
-{
-    dayNum -= 30;
-    monthNum++;
-}
-if (dayNum <= 31) //October
-{
-    goto End;
-}
-else
-{
-    dayNum -= 31;
-    monthNum++;
-}
-if (dayNum <= 30) //November
-{
-    goto End;
-}
-else
-{
-    dayNum -= 30;
-    monthNum++;
-}
-if (dayNum <= 31) //December
-{
-    goto End;
-}
-else
-{
-    dayNum -= 31;
-    monthNum++;
-}
-End:
-string monthName;
-switch (monthNum)
-{
-    case 0:
-        monthName = "January";
-        break;
-    case 1:
-        monthName = "February";
-        break;
-    case 2:
-        monthName = "March";
-        break;
-    case 3:
-        monthName = "April";
-        break;
-    case 4:
-        monthName = "May";
-        break;
-    case 5:
-        monthName = "June";
-        break;
-    case 6:
-        monthName = "July";
-        break;
-    case 7:
-        monthName = "August";
-        break;
-    case 8:
-        monthName = "September";
-        break;
-    case 9:
-        monthName = "October";
-        break;
-    case 10:
-        monthName = "November";
-        break;
-    case 11:
-        monthName = "December";
-        break;
-    default:
-        monthName = "not done yet"; break;
-}
-Console.WriteLine("{0} {1}", monthName, dayNum);
+            
